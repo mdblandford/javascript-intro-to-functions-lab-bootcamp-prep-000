206ba264d59f96112acd18d9097057793578497b
Michael Blandford <mdblandford@gmail.com>
2018-03-26 13:06:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 function shout(string){return string.toUpperCase()}
 function whisper(string){return string.toLowerCase()}
-Function logshout(string){console.log string.toAllCaps()}
+function logShout(string){console.log (string.toUpperCase)}
